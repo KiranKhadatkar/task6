d6e22ceae56ec1ede881aabd32b258bda7bdae65
Nupur <nupuragarwal521@gmail.com>
2020-08-07 10:29:35 +0530

    First issue

--- job1.sh
+++ job1.sh
@@ -1,8 +1,8 @@
-if find /host/TASKS -name task6
+if find /root/TASKS -name task6
 then
 echo "Folder already exists"
 else
-mkdir -p /host/TASKS/task6
+mkdir -p /root/TASKS/task6
 fi
 
 cp -rvf * /host/TASKS/task6
