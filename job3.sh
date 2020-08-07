e79872b169973d69968b242e8150a693045d4445
Nupur <nupuragarwal521@gmail.com>
2020-08-07 12:48:01 +0530

    fourth issue

--- job3.sh
+++ job3.sh
@@ -1,4 +1,4 @@
-status=$(curl -o /dev/null -s -w "%{http_code}" http://192.168.56.101/index.php)
+status=$(curl -o /dev/null -sw "%{http_code}" http://192.168.56.101:80/index.php)
 if [[$status == 200]]
 then
   exit 1
