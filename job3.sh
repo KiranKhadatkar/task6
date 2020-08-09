f7d2f454c430657597856f673161ef8fce6c1607
Nupur <nupuragarwal521@gmail.com>
2020-08-09 23:07:00 +0530

    commit2

--- job3.sh
+++ job3.sh
@@ -1,4 +1,4 @@
-status=$(curl -o /dev/null -sw "%{http_code}" http://192.168.99.101:31641/index.php)
+status=$(curl -o /dev/null -sw "%{http_code}" http://192.168.99.101:30805/index.php)
 if [[$status == 200]]
 then
   exit 1
